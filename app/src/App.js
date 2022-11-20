a9f801c1795149be95a24cb3471da3bbbb4fdbdc
gayoung jo <fhqht4545@gmail.com>
2022-11-20 15:40:08 +0900

    state

--- app/src/App.js
+++ app/src/App.js
@@ -23,8 +23,22 @@ const MinToHours = () => {
 }
 
 const KmToMile = () => {
+  const [amount , setAmount] = useState(0);
+  const [invert, setInvert] = useState(false)
+  const onChange = (event) => {
+    setAmount(event.target.value);
+  }
+  const reset = () => {setAmount(0)}
+  const onInvert = () => {
+    reset()
+    setInvert((current) => !current)
+  }
   return <div>
-    <h3>hi</h3>
+    <label htmlFor="Km">Km</label>
+    <input id="Km" onChange={onChange} value={invert ?  amount * 1.609 : amount} placeholder={"Km"} disabled={invert}/>
+    <label htmlFor="Miles">Miles</label>
+    <input id="Miles" onChange={onChange} value={invert ? amount : Math.round(amount / 1.609)} placeholder={"Miles"} disabled={!invert}/>
+    <button onClick={onInvert}>invert</button>
   </div>
 }
 
