f938343ce43c609f5080a85d5bd093463d582d40
gayoung jo <fhqht4545@gmail.com>
2022-11-20 16:00:13 +0900

    props연습

--- app/src/App.js
+++ app/src/App.js
@@ -1,61 +1,22 @@
 import './App.css';
-import React, {useState} from 'react'
 
-
-const MinToHours = () => {
-  const [amount , setAmount] = useState(0);
-  const [invert, setInvert] = useState(false)
-  const onChange = (event) => {
-    setAmount(event.target.value);
-  }
-  const reset = () => {setAmount(0)}
-  const onInvert = () => {
-    reset()
-    setInvert((current) => !current)
-  }
-  return <div>
-    <label htmlFor="minutes">Minutes</label>
-    <input id="minutes" onChange={onChange} value={invert ? amount * 60 : amount} placeholder={"Minutes"} disabled={invert}/>
-    <label htmlFor="hours">Hours</label>
-    <input id="hours" onChange={onChange} value={invert ? amount : Math.round(amount / 60)} placeholder={"Hours"} disabled={!invert}/>
-    <button onClick={onInvert}>invert</button>
-  </div>
-}
-
-const KmToMile = () => {
-  const [amount , setAmount] = useState(0);
-  const [invert, setInvert] = useState(false)
-  const onChange = (event) => {
-    setAmount(event.target.value);
-  }
-  const reset = () => {setAmount(0)}
-  const onInvert = () => {
-    reset()
-    setInvert((current) => !current)
-  }
-  return <div>
-    <label htmlFor="Km">Km</label>
-    <input id="Km" onChange={onChange} value={invert ?  amount * 1.609 : amount} placeholder={"Km"} disabled={invert}/>
-    <label htmlFor="Miles">Miles</label>
-    <input id="Miles" onChange={onChange} value={invert ? amount : Math.round(amount / 1.609)} placeholder={"Miles"} disabled={!invert}/>
-    <button onClick={onInvert}>invert</button>
-  </div>
-}
 
 function App() {
-  const [index, setIndex] = useState("0");
-  const onSelect = (event) => {
-    setIndex(event.target.value)
+  const Btn = ({text}) => {
+    return <button style={{
+      backgroundColor:"tomato",
+      border:0,
+      borderRadius:20,
+      padding:"10px 20px",
+      color:"white",
+    }}>
+      {text}
+    </button>
   }
   return (
     <div className="App">
-      <h1>super convert</h1>
-      <select value={index} onChange={onSelect}>
-        <option value={"0"}>MinToHours</option>
-        <option value={"1"}>KM to Miles</option>
-      </select>
-      {index === "0" ? <MinToHours /> : null}
-      {index === "1" ? <KmToMile /> : null}
+      <Btn text="Save Change" />
+      <Btn text="Continue" />
     </div>
   );
 }
