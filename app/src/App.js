a93a848cbf010aca05cd7fc3e003f9f775eb6d66
gayoung jo <fhqht4545@gmail.com>
2022-11-24 19:50:32 +0900

    7.4 Movie App part Two : 컴포넌트 분리, 라우트 사용을 위한 정리

--- app/src/App.js
+++ app/src/App.js
@@ -1,41 +1,5 @@
-import {useEffect, useState} from "react";
-
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-      getMovies();
-    }, []);
-
-  return (
-    <div className="App">
-      {loading ? <h1>loding</h1> : (
-      <div>
-        {movies.map(movie => (
-          <div key={movie.id}>
-            <img src={movie.medium_cover_image} alt={`${movie.title}poster`}/>
-            <h1>{movie.title}</h1>
-            <p>{movie.summary}</p>
-            <ul>
-              {movie.genres.map(genre => <li key={genre}>{genre}</li>)}
-            </ul>
-          </div>
-        ))}
-      </div>)}
-    </div>
-  );
+  return null
 }
 
 export default App;
