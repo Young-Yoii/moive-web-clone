b315472c31db224bb5d8a00dcfdff09790880048
gayoung jo <fhqht4545@gmail.com>
2022-11-24 19:26:36 +0900

    7.3 Movie app part one

--- app/src/App.js
+++ app/src/App.js
@@ -3,34 +3,37 @@ import {useEffect, useState} from "react";
 
 function App() {
   const [loding, setLoding] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [money, setMoney] = useState(0);
-  const [index, setIndex] = useState(0);
+  const [movies, setMovies] = useState([]);
+  
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        'https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year'
+      )
+    ).json
+    setMovies(json.data.movies)
+    setLoding(false);
+  }
 
   useEffect(() => {
-    fetch('https://api.coinpaprika.com/v1/tickers')
-      .then(response => response.json())
-      .then(json => {setCoins(json.slice(0,100)); setLoding(false);});
-  }, [])
-
-  const onChange = (event) => {
-    setMoney(event.target.value);
-  }
-
-  const onSelect = (event) => {
-    setIndex(event.target.value);
-  }
+      getMovies();
+    }, []);
 
   return (
     <div className="App">
-      <h1>The Coins {loding ? null : `(${coins.length})`}</h1>
-      {loding ? "Loding..." : <>
-      <select value={index} onChange={onSelect}>
-        {coins.map(coin => <option key={coin._id} value={coin.quotes.USD.ath_price}>{coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD</option>)}
-      </select>
-      <input placeholder="your money" value={money} onChange={onChange}/>
-      <h1>{money} 달러로 {money * index} 개로 바꿀 수 있습니다.</h1>
-      </>}
+      {loding ? <h1>loding</h1> : (
+      <div>
+        {movies.map(movie => (
+          <div key={movie.id}>
+            <img src={movie.medium_cover_img} />
+            <h1>{movie.title}</h1>
+            <p>{movie.summary}</p>
+            <ul>
+              {movie.genres.map(genre => <li key={genre}>{genre}</li>)}
+            </ul>
+          </div>
+        ))}
+      </div>)}
     </div>
   );
 }
