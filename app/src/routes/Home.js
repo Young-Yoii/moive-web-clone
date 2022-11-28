dcddd3bb321691c0ec5fd11385147462f1ec7893
gayoung jo <fhqht4545@gmail.com>
2022-11-28 21:03:41 +0900

    fix detail

--- app/src/routes/Home.js
+++ app/src/routes/Home.js
@@ -1,41 +1,46 @@
 import { useState, useEffect } from "react";
 import Movie from "../components/Movie";
+import Header from "../components/Header";
+import axios from 'axios';
+import styled from "styled-components";
 
 function Home() {
     const [loading, setLoading] = useState(true);
     const [movies, setMovies] = useState([]);
-  
-    const getMovies = async () => {
-      const json = await (
-        await fetch(
-          `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-        )
-      ).json();
-      setMovies(json.data.movies);
+
+    const getMovie = async() => {
+      const json = await axios.get( `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`);
+      setMovies(json.data.data.movies);
       setLoading(false);
-    };
-  
+    }
+
     useEffect(() => {
-        getMovies();
+      getMovie();
       }, []);
+
   
     return (
       <div className="App">
         {loading ? <h1>loding</h1> : (
         <div>
-          {movies.map(movie => (
+          <Header />
+          <MovieContainer>
+          {movies && movies.map(movie => (
             <Movie 
               key={movie.id}
               id={movie.id}
               coverImg={movie.medium_cover_image}
               title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
             />
           ))}
+          </MovieContainer>
         </div>)}
       </div>
     );
   }
   
+  const MovieContainer = styled.div`
+    display: flex;
+  `
+
   export default Home;
