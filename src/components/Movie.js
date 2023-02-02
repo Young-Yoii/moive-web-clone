0306f4ac0635ac294ca4475442872f6648419c39
Young-yoii <fhqht4545@gmail.com>
2023-02-02 23:16:58 +0900

    HOTFIX: 이미지 오류 버그 수정

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,40 +1,43 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import styles from './Movie.module.css'
+import styles from './Movie.module.css';
 import { useRef } from 'react';
 
-function Movie({id, coverImg, title, genres}) {
-    const divRef = useRef();
+function Movie({ id, coverImg, title, genres }) {
+  const divRef = useRef();
+  const wrapRef = useRef();
+  let genre = genres.slice(0, 3);
 
-    const mouseOverHandler = () => {
-      divRef.current.style = 'opacity : 1; transition: all 0.5s;'
-    }
+  const mouseOverHandler = () => {
+    divRef.current.style = 'opacity : 1; transition: all 0.5s;';
+  };
 
-    const mouseOutHandler = (e) => {
-      divRef.current.style = 'opacity : 0'
-    }
+  const mouseOutHandler = () => {
+    divRef.current.style = 'opacity : 0';
+  };
 
-    if (coverImg === undefined){
-      return null;
-    }
+  const handleError = () => {
+    wrapRef.current.style.display = 'none';
+  };
 
-    return (
-    <div className={styles.movie}>
-      <Link to ={`/movie/${id}`}>
-      <img src={coverImg} alt={`${title}poster`}/>
-      <div className={styles.movie_info_container} ref={divRef} onMouseEnter={mouseOverHandler} onMouseLeave={mouseOutHandler}>
-        <div className={styles.movie_info}>
-          <p className={styles.movie_title}>{title}</p>
-          <ul>
-            {genres.map(genre => <li key={genre}>{genre}</li>)}
-          </ul>
+  return (
+    <div className={styles.movie} ref={wrapRef}>
+      <Link to={`/movie/${id}`}>
+        <img src={coverImg} alt={`${title}poster`} onError={handleError} />
+        <div className={styles.movie_info_container} ref={divRef} onMouseEnter={mouseOverHandler} onMouseLeave={mouseOutHandler}>
+          <div className={styles.movie_info}>
+            <p className={styles.movie_title}>{title}</p>
+            <ul>
+              {genre.map((genre) => (
+                <li key={genre}>{genre}</li>
+              ))}
+            </ul>
+          </div>
+          <div className={styles.img_background}></div>
         </div>
-        <div className={styles.img_background}></div>
-      </div>
       </Link>
     </div>
-    )
-
+  );
 }
 
 Movie.propTypes = {
@@ -42,6 +45,6 @@ Movie.propTypes = {
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
-}
+};
 
 export default Movie;
