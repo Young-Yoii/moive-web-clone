f71f092f034f644285fa02de4e1a57ba07753f59
Young-yoii <fhqht4545@gmail.com>
2023-02-28 14:03:10 +0900

    fix: BrowserRouter -> HashRouter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Home from "./routes/Home";
-import Detail from "./routes/Detail";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
 import Genre from './routes/Genre';
 import './App.css';
 
 function App() {
-  return <BrowserRouter>
-    <Routes>
-      <Route path="/page/:detail" element={<Genre />} />
-      <Route path="/movie/:id" element={<Detail />} />
-      <Route path={`${process.env.PUBLIC_URL}/`} element={< Home />} />
-    </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/page/:detail" element={<Genre />} />
+        <Route path="/movie/:id" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
